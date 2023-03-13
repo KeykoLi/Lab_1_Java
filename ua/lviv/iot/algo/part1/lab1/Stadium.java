1ba3ff74ce47a3fdb74a350d1dc757f9a15460c8
KeykoLi <119291961+KeykoLi@users.noreply.github.com>
2023-03-13 16:43:42 +0200

    fix the method to create array

--- ua/lviv/iot/algo/part1/lab1/Stadium.java
+++ ua/lviv/iot/algo/part1/lab1/Stadium.java
@@ -20,18 +20,12 @@ public class Stadium {
         if (capacity >=currentAttendance + count) {
             currentAttendance += count;
         }
-        else {
-            System.out.print("Не достатня кільскість вільних місць на стадіоні");
-        }
     }
 
     public void decreaseAttendance() {
         final int DECREASE = 100;
         if (currentAttendance > DECREASE) {
             currentAttendance -= DECREASE;
-        }
-        else {
-            System.out.print("Глядачів на стадіоні меньше 100");
         }
     }
 
@@ -50,25 +44,16 @@ public class Stadium {
         return instance;
     }
 
-    @Override
-    public String toString() {
-        return "Stadium:" +
-                "\n\tName: " + name + "; " +
-                "\n\tCapacity: " + capacity + "; " +
-                "\n\tCurrent Attendance: " + currentAttendance + ";" +
-                "\n\tHome Team: " + homeTeam + "; " +
-                "\n\tAway Team:  " + awayTeam;
-    }
-
     public static void main(String[] args) {
-        Stadium[] stadium = new Stadium[4];
-        stadium[0] = new Stadium();
-        stadium[1] = new Stadium("Spartac", 3000, 2140, "Dinamo", "Shahtar");
-        stadium[2] = Stadium.getInstance();
-        stadium[3] = Stadium.getInstance();
+        Stadium[] stadium = {
+            new Stadium(),
+            new Stadium("Spartac", 3000, 2140, "Dinamo", "Shahtar"),
+            Stadium.getInstance(),
+            Stadium.getInstance()
+        };
 
-        for (int i = 0; i < stadium.length; ++i) {
-            System.out.println("" + stadium[i]);
+        for (Stadium stadiums : stadium) {
+            System.out.println(stadiums);
         }
     }
 }
